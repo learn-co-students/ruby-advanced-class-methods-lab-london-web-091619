6f5dbdfc6d8c4cbd9a4744e73c7d812527901fbb
Tim Golding <tgolding55@hotmail.com>
2019-09-18 11:33:50 +0100

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,29 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = Song.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(find_song)
+    self.all.detect{|song| song.name == find_song}
+  end
+
   def self.all
     @@all
   end
